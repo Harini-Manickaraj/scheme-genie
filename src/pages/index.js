db36822b70229f32476885eb4e88a707b80142ab
Harini-Manickaraj <2403717673822015@cit.edu.in>
2025-06-10 19:07:51 +0530

    home page added to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ export default function Home() {
       <h1>Welcome to Scheme Genie</h1>
       <p>Your personalized government scheme recommender system.</p>
       <nav>
+        <Link href="/">Home</Link> |
         <Link href="/about">About</Link> |
         <Link href="/contact">Contact</Link> |
         <Link href="/schemes">Schemes</Link>
