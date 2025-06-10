220c30f5063bec0b6aabf03d950947d85c027c9d
Harini-Manickaraj <2403717673822015@cit.edu.in>
2025-06-10 19:09:50 +0530

    Home page added to about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,8 @@ export default function About() {
       <h1>About Scheme Genie</h1>
       <p>We aim to help citizens find the most relevant government schemes.</p>
       <nav>
-        <Link href="/">About</Link> |
+        <Link href="/">Home</Link> |
+        <Link href="/about">About</Link> |
         <Link href="/contact">Contact</Link> |
         <Link href="/schemes">Schemes</Link>
       </nav>
